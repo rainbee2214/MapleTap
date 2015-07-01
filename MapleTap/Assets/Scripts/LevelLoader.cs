5487bf5ccd59bb9dadbed0c8b1a70cd8236a42a2
Joel Kuntz <joel.kuntz.92@gmail.com>
2015-07-01 17:03:34 -0300

    Setup level loaded for menu and stats

---
+++ MapleTap/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelLoader : MonoBehaviour
+{
+    public string defaultLevel = "Menu";
+    public bool clickToLoad = true;
+
+    void Update()
+    {
+        if (clickToLoad && Input.GetButtonDown("Action")) LoadLevel(defaultLevel);
+    }
+
+    public void LoadLevel(string l)
+    {
+        Application.LoadLevel(l);
+    }
+}
