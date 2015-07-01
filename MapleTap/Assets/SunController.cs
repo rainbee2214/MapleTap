bdcaa86383dc10a28121addcad6793c20e014004
Joel Kuntz <joel.kuntz.92@gmail.com>
2015-07-01 17:41:10 -0300

    Setup sun rotation

---
+++ MapleTap/Assets/SunController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SunController : MonoBehaviour
+{
+    public float minYRotation = 70.0f,
+                 maxYRotation = -70.0f;
+
+    void Start()
+    {
+        InvokeRepeating("RotateForDay", 0.0f, GameController.controller.DayLength);
+    }
+
+    void RotateForDay()
+    {
+        transform.rotation = Quaternion.Euler(transform.rotation.x,
+                                              Mathf.Lerp(minYRotation, maxYRotation, GameController.controller.Day / 365),
+                                              transform.rotation.z);
+    }
+
+}
