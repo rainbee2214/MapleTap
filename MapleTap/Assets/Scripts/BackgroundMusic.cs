0a80a5c41c9a3829e802c31afe64bc3094a44240
rainbee2214 <rainbee2214@gmail.com>
2015-07-01 22:51:42 -0300

    Done for the night. Happy Canajam :)

---
+++ MapleTap/Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundMusic : MonoBehaviour {
+    public static BackgroundMusic bgMusic;
+
+    void Awake()
+    {
+        if (bgMusic == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            bgMusic = this;
+        }
+        else if (bgMusic != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
